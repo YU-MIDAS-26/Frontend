84f4cacef9d3c382350cab407c6a2220f545388f
전유진 <123734118+zzzin18@users.noreply.github.com>
2026-04-02 17:13:51 +0900

    [추가]프로젝트 초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+//import { useState } from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h1>Midas 2026 Frontend</h1>
+    </div>
+  );
+}
+
+export default App;
