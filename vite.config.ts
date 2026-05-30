b4c40dbcf1b2fd2ee7d099a8411d9d9842515c4c
전유진 <123734118+zzzin18@users.noreply.github.com>
2026-05-30 14:38:28 +0900

    [추가] 프록시 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080", // 백엔드 스프링부트 로컬 포트 번호
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
